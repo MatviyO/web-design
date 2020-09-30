2096cb81236bb87e0e64b414baa2440b1a7aaa71
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-09-30 21:12:45 +0300

    fgf

---
+++ animationScroll/index.js
@@ -0,0 +1,26 @@
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    function animOnScroll(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientReact(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+}
